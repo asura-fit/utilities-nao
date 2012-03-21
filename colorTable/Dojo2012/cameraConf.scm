9ac502d629048d6b56794a38f145534bd3cf7c78
matsumo <devnull@localhost>
2012-03-21 15:59:44 +0900

    カラーテーブル追加した。

---
+++ colorTable/Dojo2012/cameraConf.scm
@@ -0,0 +1,11 @@
+;camera settings
+
+(vc-set-param AEC 1)
+
+(vc-set-param Brightness 128)
+(vc-set-param Contrast 64)
+(vc-set-param Gain 6)
+(vc-set-param Exposure 450)
+(vc-set-param RedChroma 80)
+(vc-set-param BlueChroma 130)
+
